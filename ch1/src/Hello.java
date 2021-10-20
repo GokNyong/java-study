8473c40b317a77439e2898af24dcfff5e174539b
chanye_120 <fordynamic@gmail.com>
2021-10-20 23:05:51 +0900

    initial commit

---
+++ ch1/src/Hello.java
@@ -0,0 +1,26 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shift+L 단축키 전체 목록보기
+		// ctrl+ +,-
+		// ctrl+D 한줄삭제
+		// ctrl+alt+down 행단위 복사
+		// alt+shift+A 멀티컬럼 편집
+		// alt+up, down 행단위 이동
+		// ctrl +i, 자동 들여쓰기
+		// ctrl + /, 주석처리
+		/* 여러줄 주석 시작
+		System.out.println("Hello world.");
+		System.out.println("Hello world.");
+		System.out.println("Hello world.");
+		끝 -> */
+		// ctrl+space 자동완성 ex)sysout
+		System.out.println();
+		System.out.println("Hello world.");
+		System.out.println("Hello world.");
+	
+	}
+
+}
